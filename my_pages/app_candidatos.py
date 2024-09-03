8d5ea362c781985f0f1c65684b1ea48534a8359f
Yuri Dimitri Ribeiro Lima <dimitri.limaf@gmail.com>
2024-09-03 17:39:10 -0300

    Adicionando texto no app_candidatos

--- my_pages/app_candidatos.py
+++ my_pages/app_candidatos.py
@@ -91,6 +91,8 @@ def app_candidatos():
 
     st.title('Dados Eleitorais - Candidatos - 2024')
     st.subheader("""Essa análise contem análise de dados inspirada em uma notícia do G1, a qual fala sobre o perfil geral dos candidatos para eleições municipais de 2024. Esse projeto tem o intuito de fazer um 'checagem de dados' tanto para treinar o pensamento analítico e quando outras habilidades para um futuro profissional estatístico.""")
+
+    # GRÁFICO DE BARRAS MOSTRANDO ÀS DIFERENÇAS DE GÊNERO
     genero = candidatos_20_24.groupby(
         ['Ano de eleição', 'Gênero'], observed=False).agg(count=('Gênero', 'size'))
     genero = genero.reset_index()
@@ -101,9 +103,10 @@ def app_candidatos():
               fontdict={'fontsize': 20})
     plt.xlabel('Gênero', fontdict={'fontsize': 20})
     plt.ylabel('Contagem', fontdict={'fontsize': 20})
-    st.markdown('# Gráfico de Barras - G')
+    st.markdown('Iniciamos com o gráfico demonstrando a diferenças entre 2020 e 2024 sobre a quantidade de candidatos pelo gênero. É possível notar que temos reduções de registros em ambos os gêneros. Contudo, ainda temos um número superior de homens em relação às mulheres em ambos os anos de eleições municipais. Em 2020 alguns candidatos colocaram a opção não divulgável, o que podemos supor que pode ser apenas por esquecimento na hora do registro ou porquê o candidato não se identificava com nenhum dos gêneros, mas em 2024 (ano dessa análise) isso não ocorreu.')
     st.pyplot(fig=fig_bar, use_container_width=True)
 
+    # GRÁFICO DE BARRAS HORIZONTAL MOSTRANDO A QUANTIDADE DE REGISTROS POR RAÇA
     fig_barh, ax = plt.subplots(1, 2, figsize=(20, 8))
 
     cor_raca = candidatos_20_24.groupby(
@@ -123,9 +126,10 @@ def app_candidatos():
         ax[1].margins(x=0.3)
 
     fig_barh.suptitle('Contagem de Raças entre 2020 e 2024', fontsize=20)
-    st.markdown('# Gráfico de Barras H - R')
+    st.markdown('Continuando a análise, seguimos verificando quais são as raças mais frequentes nos dados de 2020 e 2024. Segundo a matéria temos em 2024 diz que 53% dos candidatos se identificavam como negro ou pardo, mas como podemos ver no gráfico podemos ver que a maior quantidade de registros de raça são de candidatos que se identificam como Branco. ')
     st.pyplot(fig=fig_barh, use_container_width=True)
 
+    # GRÁFICO DE NUVENS DE PALAVRAS PARA VERIFICAR A FREQUENCIA DE ESTADO CIVIL
     estado_civil = candidatos_20_24.groupby(
         ['Ano de eleição', 'Estado civil'], observed=False).agg(count=('Estado civil', 'size'))
     counts_2020 = estado_civil.loc['2020'].to_dict()['count']
@@ -151,9 +155,11 @@ def app_candidatos():
 
     fig_cloud.suptitle(
         'Nuvem de palavras - Estado Civil (2020 - 2024)', fontsize=20)
-    st.markdown('# Gráfico de Nuvem de Palavras')
+    st.markdown(
+        'De acordo com a matéria do G1, o que tivemos mais frequência de pessoas casadas e tanto em 2020 e 2024 e as informações estão coerente.')
     st.pyplot(fig=fig_cloud, use_container_width=True)
 
+    # GRÁFICO DE BARRAS SOBRE A OCUPAÇÃO DOS CANDIDATOS - RANK
     ocupacao = candidatos_20_24.groupby(
         ['Ano de eleição', 'Ocupação'], observed=False).agg(count=('Ocupação', 'size'))
     ocupacao_2020 = ocupacao.loc['2020']['count'].rank(
@@ -184,9 +190,10 @@ def app_candidatos():
 
     fig_rank.suptitle(
         'Profissões mais frequentes entre 2020 e 2024', fontsize=20)
-    st.markdown('# Gráfico de Rank')
+    st.markdown('Nesse momento da análise, foi notado uma diferença de resultados relacionados a esse recorte. Na matéria do G1, foi abordado que a ocupação mais frequente entre os candidatos era empresário. Contudo, como pode ser observado no gráfico de rank, que a ocupação mais frequente é designada como "OUTROS", o que leva a pensar como questionamento porque a matéria designou "OUTRO" como empresários. Talvez possa ter considerado candidatos que tenham como ocupação um trabalho no modelo de pessoa júridica. Outra tópico importante a ser analisado é que em 2024 temos como o 5° lugar vereadores, o que indica que muitas pessoas esse ano estão tentando reeleição.')
     st.pyplot(fig=fig_rank, use_container_width=True)
 
+    # GRÁFICO DE BARRAS - FAIXA ETÁRIA
     faixa_etaria = candidatos_20_24.groupby(
         ['Ano de eleição', 'Faixa etária'], observed=False).agg(count=('Faixa etária', 'size'))
     formacao = candidatos_20_24.groupby(['Ano de eleição', 'Grau de instrução'], observed=False).agg(
@@ -209,8 +216,10 @@ def app_candidatos():
         ax[1].set_xticklabels(ax[1].get_xticklabels(), rotation=45)
     fig_idade.suptitle(
         'Faixas Etárias mais frequentes entre 2020 e 2024', fontsize=20)
-    st.markdown('# Gráfico de Barras II -Idade')
+    st.markdown('Continuando a checagem, em 2020 temos mais candidatos dentro da faixa etária de "40 a 44 anos" dando espaço em 2024 para a faixa etária de "45 a 49 anos". Na matéria foi feita uma média entre as duas idades do intervalo dizendo que seria 46 anos, redondando para baixo, pois calculando entre as duas temos 47 anos. Pode parecer simples, mas dizer 46 anos seria a mais frequente sugere que a equipe do G1 envolvida nessa matéria teve acesso às idades e pode fazer uma mediana dos dados. Pois, os dados obtidos para essa análise no site do TSE não mostram os dados de idades como números inteiros, mas só a título de curiosidade.')
     st.pyplot(fig=fig_idade, use_container_width=True)
+
+    # GRÁFICO DE DISPERSÃO DOS DADOS SOBRE ESCOLARIDADE
 
     fig_scatter, ax = plt.subplots(2, 1, figsize=(20, 10))
 
@@ -225,20 +234,18 @@ def app_candidatos():
     ax[0].set_xticklabels(ax[0].get_xticklabels(), rotation=45)
     ax[1].set_xticklabels(ax[1].get_xticklabels(), rotation=45)
 
-    # Ajustando os rótulos dos eixos
     ax[0].set_xlabel('Grau de Instrução', fontsize=14)
     ax[0].set_ylabel('Contagem de Candidatos', fontsize=14)
     ax[0].margins(x=0.2, y=0.2)
     ax[1].set_xlabel('Grau de Instrução', fontsize=14)
     ax[1].set_ylabel('Contagem de Candidatos', fontsize=14)
     ax[1].margins(x=0.2, y=0.2)
-    # Adicionando título e subtítulo ao gráfico
+
     fig_scatter.suptitle(
         'Distribuição de Formação de Candidatos em 2020 e 2024', fontsize=20)
     fig_scatter.text(
         0.2, 0.85, 'Análise comparativa do \n grau de instrução\n dos candidatos nos anos de 2020 e 2024', fontsize=12, ha='center')
 
-    # Ajustando a legenda
     handles, labels = ax[0].get_legend_handles_labels()
     ax[0].legend(handles, labels, title="Tamanho das Bolhas",
                  bbox_to_anchor=(1.05, 1), loc='upper left')
@@ -246,9 +253,10 @@ def app_candidatos():
                  bbox_to_anchor=(1.05, 1), loc='upper left')
 
     fig_scatter.tight_layout()
-    st.markdown('# Gráfico de Dispersão')
+    st.markdown('Quanto a escolaridade dos candidatos, realmente temos mais registros para "ENSINO MÉDIO COMPLETO" em ambos os anos confirmando os dados dispostos na matéria. Um dado interessante a ser analisado é que o número de pessoas com ensino superior completo teve um aumento, demonstrando uma possível preocupação dos candidatos com aprimoramento técnico.(esperemos)')
     st.pyplot(fig=fig_scatter, use_container_width=True)
 
+    # GRÁFICO DE BARRAS - DIFERENÇA DE REGISTROS POR SIGLA
     fig_siglas = plt.figure(figsize=(20, 8))
     contagem_siglas = candidatos_20_24.groupby(
         ['Ano de eleição', 'Sigla partido'], observed=False).agg(count=('Sigla partido', 'size'))
@@ -261,11 +269,11 @@ def app_candidatos():
     plt.title('Diferença de Entradas entre 2020 a 2024', fontsize=20)
     plt.margins(y=0.05)
 
-    st.markdown('# Gráfico de Siglas')
+    st.markdown('Para tornar um pouco mais rica a análise desse relatório, foi produzido esse gráfico mostrando a diferença de registro entre os anos de 2020 e 2024 para as principais siglas de partidos polítcos do pais. Pode ser observado que as siglas que mais tiveram entradas, respectivamente foi SOLIDARIEDADE, PP e PL.')
     st.pyplot(fig=fig_siglas, use_container_width=True)
 
     # gráfico folium
-    st.markdown('# Gráfico de Mapa')
+    st.markdown('Para entender um pouco mais das siglas que tiveram mais registro entre 2020 e 2024. Acesse o gráfico abaixo escolhendo o estado.')
 
     contagem_estados = candidatos_20_24.groupby(
         ['Ano de eleição', 'UF', 'Sigla partido'], observed=False).agg(count=('UF', 'size'))
@@ -293,3 +301,6 @@ def app_candidatos():
                       popup=f'O Estado de {estado} teve em 2020 {total_estado_2020.iloc[0]} registros e o partido que teve mais registros foi {sigla_max_2020.iloc[0]} representando {porcentagem_2020.iloc[0]*100:.2f}% do total de registros. Já  em 2024 ocorreram {total_estado_2024.iloc[0]} registros e o partido que teve mais registros foi {sigla_max_2024.iloc[0]} representando {porcentagem_2024.iloc[0]*100:.2f}% do total de registros',
                       icon=folium.Icon(icon="glyphicon glyphicon-tasks", prefix="glyphicon")).add_to(mapa_ufs)
     st_folium(mapa_ufs, use_container_width=True)
+
+    st.markdown('# Conclusão')
+    st.text('A matéria do G1 apresentou os dados de acordo com a realidade deles, contudo, pode-se perceber alguns viéses, provalvemente fruto da própria pauta editoral. Algumas outras insights desse projeto vão estar dispostos dentro do reposítorio do github e da apresentação em vídeo do autor do projeto. Foi de escolha do autor se manter nesse momento dentro da checagem de fatos e dados da matéria jornalística, mas está aberto a quaisquer contribuições, insights e colaborações para tornar esse projeto cada vez mais rica para as próximas eleições.')
